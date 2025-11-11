7ecaa9f0f109ede851a77bcd955be118783fd9d6
ishaansheth08-droid <ishaansheth08@gmail.com>
2025-11-12 01:04:07 +0530

    Update 2.cpp

--- Assignment 7/2.cpp
+++ Assignment 7/2.cpp
@@ -20,8 +20,8 @@ public:
 
 int main() {
     Sum s;
-    int marks[10] = {45, 56, 67, 78, 89, 90, 76, 88, 92, 85};
-    float grades[5] = {9.2, 8.7, 9.5, 8.9, 9.0};
+    int marks[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    float grades[5] = {6.7, 8.7, 9.7, 7.7, 5.7};
 
     cout << "Sum of 10 student marks: " << s.total(marks, 10) << endl;
     cout << "Sum of 5 student grade points: " << s.total(grades, 5) << endl;
