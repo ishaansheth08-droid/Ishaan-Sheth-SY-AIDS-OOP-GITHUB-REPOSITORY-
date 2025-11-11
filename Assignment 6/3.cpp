4344cb0f29f128c81527e27d002c8d83b98858b3
ishaansheth08-droid <ishaansheth08@gmail.com>
2025-11-12 00:48:58 +0530

    Update 3.cpp

--- Assignment 6/3.cpp
+++ Assignment 6/3.cpp
@@ -1,68 +1,45 @@
 #include <iostream>
 using namespace std;
 
-class Person{
-    protected:
-    string name;
-    int age;
-    public:
-    void acceptPer(){
-        cout << "Enter name: " <<endl; 
-        cin >> name; 
-        cout << "Enter age: " <<endl; 
-        cin >> age; 
-    }
-
-};
-
-class Student : public Person{
-    int roll;
-    float per;
-    public:
-    void acceptStud(){
-        cout << "Enter roll number: " <<endl; 
-        cin >> roll;
-        cout << "Enter Percentage: " <<endl; 
-        cin >> per;  
-    }
-
-    void displayStud(){
-        cout << "Name: " << name << endl; 
-        cout << "Age: " << age << endl;
-        cout << "Roll Number: " << roll << endl; 
-        cout << "Percentage: " << per <<endl; 
+class Person {
+protected:
+    string name; int age;
+public:
+    void getPerson() {
+        cout << "Enter name & age: ";
+        cin >> name >> age;
     }
 };
 
-class Staff : public Person{
-    int emp_id;
-    string subject;
-    public:
-    void acceptStaff(){
-        cout << "Enter employee ID: " <<endl; 
-        cin >> emp_id;
-        cout << "Enter subject: " <<endl; 
-        cin >> subject;  
+class Student : public Person {
+    int roll; float per;
+public:
+    void getStudent() {
+        cout << "Enter roll & %: ";
+        cin >> roll >> per;
     }
-    void displayStaff(){
-        cout << "Name: " << name << endl; 
-        cout << "Age: " << age << endl;
-        cout << "Employee ID: " << emp_id << endl; 
-        cout << "Subject taught: " << subject <<endl; 
+    void showStudent() {
+        cout << "\nStudent\nName: " << name << "\nAge: " << age
+             << "\nRoll: " << roll << "\n%: " << per << endl;
     }
 };
- 
-int main(){
-    Student s;
-    Staff t;
 
-    s.acceptPer();
-    s.acceptStud();
-    t.acceptPer();
-    t.acceptStaff();
+class Staff : public Person {
+    int id; string sub;
+public:
+    void getStaff() {
+        cout << "Enter ID & subject: ";
+        cin >> id >> sub;
+    }
+    void showStaff() {
+        cout << "\nStaff\nName: " << name << "\nAge: " << age
+             << "\nID: " << id << "\nSubject: " << sub << endl;
+    }
+};
 
-    s.displayStud();
-    t.displayStaff();
-    
-    return 0;
+int main() {
+    Student s; Staff t;
+    s.getPerson(); s.getStudent();
+    t.getPerson(); t.getStaff();
+    s.showStudent(); t.showStaff();
 }
