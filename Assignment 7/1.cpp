9004b7f857f9ce2fce366d6b1951cef860c32dfd
ishaansheth08-droid <ishaansheth08@gmail.com>
2025-11-12 00:57:23 +0530

    Update 1.cpp

--- Assignment 7/1.cpp
+++ Assignment 7/1.cpp
@@ -14,7 +14,7 @@ public:
 
 int main() {
     Area a;
-    cout << "Area of Laboratory (Rectangular): " << a.calculate(10, 6) << endl;
+    cout << "Area of Laboratory (Rectangular): " << a.calculate(6, 7) << endl;
     cout << "Area of Classroom (Square): " << a.calculate(5) << endl;
     return 0;
 }
